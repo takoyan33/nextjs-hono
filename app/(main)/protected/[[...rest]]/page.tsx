349936562e6305202f37978d11f2533f6ced1c31
abeshumpei <harrier2070@gmail.com>
2025-01-24 21:25:17 +0900

    fix: protectedの修正

---
+++ app/(main)/protected/[[...rest]]/page.tsx
@@ -0,0 +1,35 @@
+import { UserButton, UserProfile } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import Image from "next/image";
+
+const ProtectedPage = async () => {
+  const { userId } = await auth();
+  const user = await currentUser();
+
+  return (
+    <div className="flex flex-col p-10">
+      <div className="flex items-center gap-x-2">
+        <UserButton />
+        <p>User ID: {userId}</p>
+      </div>
+      <ul className="flex flex-col p-6">
+        <li>
+          User Name: {user?.firstName} {user?.lastName}
+        </li>
+        <li>User Email: {user?.emailAddresses?.[0].emailAddress}</li>
+        <li className="flex gap-x-2">
+          User image:{" "}
+          <Image
+            src={user?.imageUrl || ""}
+            width={30}
+            height={30}
+            alt="User Image"
+          />
+        </li>
+      </ul>
+      <UserProfile />
+    </div>
+  );
+};
+
+export default ProtectedPage;
